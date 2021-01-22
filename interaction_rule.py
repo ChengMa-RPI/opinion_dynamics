d96c2acf44218addc0e65828a6cfe1dd8a4992cf
Cheng <phynk_macheng@outlook.com>
2021-01-22 15:33:17 -0500

    interaction_rule.py -- stable states of 3 opinions

---
+++ interaction_rule.py
@@ -0,0 +1,255 @@
+import os
+os.environ['OPENBLAS_NUM_THREADS'] ='1'
+
+import numpy as np 
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+import time 
+import pandas as pd
+import multiprocessing as mp
+from itertools import combinations
+
+cpu_number = 10
+
+def transition_rule(s1, s2):
+    """states after interaction (speaker-listenser) 7 states: A, B, C, AB, AC, BC, ABC
+        
+
+    :s1: TODO
+    :s2: TODO
+    :returns: TODO
+
+    """
+    result = []
+    if s1.islower() and s2.islower():
+        sf1 = s1
+        sf2 = s2
+        result.append([sf1, sf2])
+    elif s1.islower() and s2.isupper():
+        sf1 = s1
+        v = s1.upper()
+        if v in s2:
+            sf2 = v
+        else:
+            sf2 = v + s2
+            sf2 = ''.join(sorted(sf2))
+        result.append([sf1, sf2])
+    elif s1.isupper() and s2.islower():
+        sf2 = s2
+        u = s2.upper()
+        for v in s1:
+            if v != u:
+                sf1 = s1
+            else:
+                sf1 = v
+            result.append([sf1, sf2])
+    else:
+        for v in s1:
+            if v in s2:
+                sf1 = v
+                sf2 = v
+            else:
+                sf1 = s1
+                sf2 = v + s2
+
+            sf2 = ''.join(sorted(sf2))
+            result.append([sf1, sf2])
+    return result
+
+def all_state(number_opinion):
+    """TODO: Docstring for change_rule.
+
+    :number_opinion: TODO
+    :returns: TODO
+
+    """
+
+    single_state = [chr(state) for state in range(ord('A'), ord('A') + number_opinion)]
+    possible_state = single_state.copy()
+    possible_state.extend([committed.lower() for committed in single_state])
+    for length in range(2, number_opinion):
+        for state in combinations(single_state, length):
+            possible_state.append(''.join(state))
+    possible_state.append(''.join(single_state))
+    return possible_state
+
+def change_rule(number_opinion):
+    """TODO: Docstring for change_rule.
+
+    :number_opinion: TODO
+    :returns: TODO
+
+    """
+    possible_state = all_state(number_opinion)
+    transition_before_list = []
+    transition_after_list = []
+    for s1 in possible_state:
+        for s2 in possible_state:
+            transition_before_list.append([s1, s2])
+            transition_after_list.append(transition_rule(s1, s2))
+    interaction_num = len(transition_after_list)
+    change_matrix = np.zeros((interaction_num, len(possible_state)))
+    for i in range(interaction_num):
+        transition_after = transition_after_list[i]
+        transition_before = transition_before_list[i]
+        len_result = len(transition_after)
+        for x in transition_before:
+            index = possible_state.index(x)
+            change_matrix[i, index] -= 1
+            
+        for one_result in transition_after:
+            for x in one_result:
+                index = possible_state.index(x)
+                change_matrix[i, index] += 1/len_result
+    return change_matrix
+
+def mf_ode(x, t, number_opinion):
+    """TODO: Docstring for mf_ode.
+
+    :arg1: TODO
+    :returns: TODO
+
+    """
+    length = len(x)
+    x_matrix = x.reshape(length, 1) * x.reshape(1, length)
+    coefficient = change_rule(number_opinion)
+    c_matrix = coefficient.reshape(length, length, length).transpose(2, 0, 1) 
+    dxdt = np.sum(c_matrix * x_matrix, (1, 2))
+    return dxdt
+
+def ode_stable(number_opinion, committed_fraction, single_fraction):
+    """TODO: Docstring for ode_stable.
+
+    :number_opinion: TODO
+    :committed_fraction: TODO
+    :returns: TODO
+
+    """
+    start = 0
+    end = 100
+    difference = 1
+    single_fraction1 = single_fraction
+    mixed_fraction = np.zeros((2**number_opinion -1 -number_opinion))
+    while abs(difference) > 1e-10:
+        t = np.arange(start, end, 0.01)
+        initial_state = np.hstack(([single_fraction1, committed_fraction, mixed_fraction]))
+        result = odeint(mf_ode, initial_state, t, args=(number_opinion,))
+        single_fraction2 = result[-1, :number_opinion]
+        mixed_fraction = result[-1, 2*number_opinion:]
+        difference = sum(single_fraction2 - single_fraction1)
+        single_fraction1 = single_fraction2
+    return single_fraction1
+    
+def bifurcation(number_opinion):
+    """TODO: Docstring for bifurcation.
+
+    :number_opinion: TODO
+    :returns: TODO
+
+    """
+    region1 = []
+    if number_opinion == 2:
+        for p in np.arange(0, 0.3, 0.01):
+            for q in np.arange(0, 0.3, 0.01):
+                committed_fraction = np.array([p, q])
+                single_fraction1 = np.array([1-p-q, 0])
+                result1 = ode_stable(number_opinion, committed_fraction, single_fraction1)
+
+                single_fraction2 = np.array([0, 1-p-q])
+                result2 = ode_stable(number_opinion, committed_fraction, single_fraction2)
+
+                if sum(abs(result2 -result1))> 1e-3:
+                    region1.append([p, q])
+                print(p, q, result1, result2)
+
+    if number_opinion == 3:
+        for p in np.arange(0, 0.3, 0.01):
+
+            committed_fraction = np.array([p, p, p])
+            single_fraction1 = np.array([1-3*p, 0, 0])
+            result1 = ode_stable(number_opinion, committed_fraction, single_fraction1)
+
+            single_fraction2 = np.array([0, 0, 1-3*p])
+            result2 = ode_stable(number_opinion, committed_fraction, single_fraction2)
+
+            if sum(abs(result2 -result1))> 1e-3:
+                region1.append([p])
+            print(p, result1, result2)
+
+
+    if number_opinion > 3:
+        for p in np.arange(0, 0.3, 0.01):
+
+            committed_fraction = np.ones(number_opinion) * p
+            single_fraction1 = np.hstack((1-number_opinion * p, np.zeros((number_opinion-1))))
+            result1 = ode_stable(number_opinion, committed_fraction, single_fraction1)
+
+            single_fraction2 = np.hstack((np.zeros((number_opinion-1)), 1-number_opinion * p))
+            result2 = ode_stable(number_opinion, committed_fraction, single_fraction2)
+
+            if sum(abs(result2 -result1))> 1e-3:
+                region1.append([p])
+            print(p, result1, result2)
+
+
+                
+    return region1
+
+def attractors(number_opinion, committed_fraction, des_file):
+    """TODO: Docstring for attractors.
+
+    :number_opinion: TODO
+    :committed_fraction: TODO
+    :single_fraction: TODO
+    :returns: TODO
+
+    """
+
+    print(committed_fraction)
+    attractor_list = []
+    uncommitted_fraction = 1 - sum(committed_fraction)
+    for a in np.arange(0, uncommitted_fraction, 0.1):
+        for b in np.arange(0, uncommitted_fraction-a, 0.1):
+            c = uncommitted_fraction-a-b
+            single_fraction = np.array([a, b, c])
+            attractor = ode_stable(number_opinion, committed_fraction, single_fraction)
+            print(a, b, c, attractor)
+            attractor_list.append(attractor)
+    attractor_unique = np.unique(np.round(np.vstack((attractor_list)), 4), axis=0)
+    data = np.hstack((committed_fraction, np.ravel(attractor_unique)))
+    df_data = pd.DataFrame(data.reshape(1, len(data)))
+    df_data.to_csv(des_file, index=None, header=None, mode='a')
+
+    return attractor_unique
+
+def parallel_attractors(number_opinion, committed_fraction_list):
+    """TODO: Docstring for parallel_attractors.
+
+    :number_opinion: TODO
+    :committed_fraction: TODO
+    :returns: TODO
+
+    """
+    des = '../data/'
+    if not os.path.exists(des):
+        os.makedirs(des)
+    des_file = des + f'num_opinion={number_opinion}_stable.csv'
+    p = mp.Pool(cpu_number)
+    p.starmap_async(attractors, [(number_opinion, committed_fraction, des_file) for committed_fraction in committed_fraction_list]).get()
+    p.close()
+    p.join()
+
+    return None
+
+number_opinion = 3
+
+committed_fraction_list = []
+for p in np.arange(0, 0.15, 0.05):
+    for q in np.arange(0, min(1-p, 0.15), 0.05):
+        for r in np.arange(0, min(1-p-q, 0.15), 0.05):
+            committed_fraction = np.array([p, q, r])
+            committed_fraction_list.append(committed_fraction)
+committed_fraction_list = np.vstack((committed_fraction_list))
+
+parallel_attractors(number_opinion, committed_fraction_list)
+t2 = time.time()
